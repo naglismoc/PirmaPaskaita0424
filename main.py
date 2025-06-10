02346b879385ee932a949d0a6460fdbe28b6374b
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2025-06-10 20:10:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -133,6 +133,6 @@ for y in range(1, 11):
 new_array = ["labas","vakaras","studentai"]
 
 num_arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-print("nu ka, pavyko?:)")
+print("nu ka, pavyko?:) dabar jau???")
 
 print("labas ir ate")
